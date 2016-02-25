afc36eb0d40c631d90cdaa1b33c235878d1c56fc
Eric Lifka <eric.lifka@gmail.com>
2016-02-24 23:13:47 -0500

    don't need to wrap the request in a new promise since ic-ajax returns a real promise

--- app/authenticators/username-password.js
+++ app/authenticators/username-password.js
@@ -1,8 +1,6 @@
+import Ember from'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 import { request } from 'ic-ajax';
-import Ember from'ember';
-const { Promise } = Ember.RSVP;
-const { $ } = Ember;
 
 export default Base.extend({
   restore(data) {
@@ -11,11 +9,9 @@ export default Base.extend({
     });
   },
   authenticate(username, password) {
-    return new Promise(function (resolve, reject) {
-      request({
-        url: `/login?username=${username}&password=${password}`,
-        type: `POST`
-      }).then(resolve, reject);
+    return request({
+      url: `/login?username=${username}&password=${password}`,
+      type: `POST`
     });
   },
   invalidate(data) {
