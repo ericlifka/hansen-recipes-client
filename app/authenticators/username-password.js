eea3c3f08b1947ee106c1e0ec80d72231f63ea2d
Eric Lifka <eric.lifka@gmail.com>
2016-02-28 11:18:09 -0500

    more es6 silliness

--- app/authenticators/username-password.js
+++ app/authenticators/username-password.js
@@ -3,13 +3,13 @@ import Base from 'ember-simple-auth/authenticators/base';
 import { request } from 'ic-ajax';
 
 export default Base.extend({
-  restore() {
-    return request({ url: '/session' });
-  },
-  authenticate(username, password) {
-    return request({ url: '/login', type: 'POST', data: { username, password } });
-  },
-  invalidate() {
-    return request({ url: '/logout', type: 'POST' });
-  }
+  restore: () => request({ url: '/session' }),
+
+  invalidate: () => request({ url: '/logout', type: 'POST' }),
+
+  authenticate: (username, password) => request({
+    url: '/login',
+    type: 'POST',
+    data: { username, password }
+  })
 });
