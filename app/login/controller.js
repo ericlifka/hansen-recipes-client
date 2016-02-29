9310b652e24def9dabb079197129a427c0fa5efd
Eric Lifka <eric.lifka@gmail.com>
2016-02-28 23:42:19 -0500

    add logic for registering a new user

--- app/login/controller.js
+++ app/login/controller.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { request } from 'ic-ajax';
 const { Controller, inject } = Ember;
 
 export default Controller.extend({
@@ -8,14 +9,37 @@ export default Controller.extend({
 
   actions: {
     processForm() {
-      let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:username-password', identification, password)
-        .catch(reason => {
-          this.set('errorMessage', reason.error || reason);
-        });
+      if (this.get('register')) {
+        this.processRegistration();
+      } else {
+        this.processLogin();
+      }
     },
     toggleRegistration() {
       this.toggleProperty('register');
     }
+  },
+
+  processLogin() {
+    const { username, password } = this.getProperties('username', 'password');
+    this.get('session').authenticate('authenticator:username-password', username, password)
+      .catch(reason => {
+        this.set('errorMessage', reason.error || reason);
+      });
+  },
+
+  processRegistration() {
+    const { username, password, passwordRepeat } = this.getProperties('username', 'password', 'passwordRepeat');
+
+    request({ url: '/register', type: 'POST', data: { username, password } })
+      .then(result => {
+        this.set('errorMessage', 'Registration successful, please login to continue');
+        this.set('register', false);
+        this.set('password', '');
+        this.set('passwordRepeat', '');
+      })
+      .catch(error => {
+        this.set('errorMessage', reason.error || reason);
+      });
   }
 });
