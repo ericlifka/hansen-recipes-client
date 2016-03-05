14964b864b51e499f2cede5dd824d5003844e509
Eric Lifka <eric.lifka@gmail.com>
2016-03-05 08:54:03 -0500

    add an auth required route for each of the app pages, so they aren't accessible directly

---
+++ app/routes/favorites.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+const { Route } = Ember;
+
+export default Route.extend(AuthenticatedRouteMixin);
