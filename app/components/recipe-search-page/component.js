e94b80660aac7b136fdc05f46ba6a8434e205d08
Eric Lifka <eric.lifka@gmail.com>
2016-03-06 23:54:51 -0500

    button controls for the panels. not elegant, but functional

--- app/components/recipe-search-page/component.js
+++ app/components/recipe-search-page/component.js
@@ -2,5 +2,20 @@ import Ember from 'ember';
 const { Component, computed } = Ember;
 
 export default Component.extend({
-  classNames: ['recipe-search-page']
+  classNames: ['recipe-search-page'],
+
+  actions: {
+    openTagsFilterPanel() {
+      this.set('tagsPanel', true);
+      this.set('ingredientsPanel', false);
+    },
+    openIngredientsFilterPanel() {
+      this.set('ingredientsPanel', true);
+      this.set('tagsPanel', false);
+    },
+    closeFilterPanels() {
+      this.set('tagsPanel', false);
+      this.set('ingredientsPanel', false);
+    }
+  }
 });
