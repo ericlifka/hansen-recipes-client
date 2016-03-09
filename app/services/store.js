d70a53680f67fa4cfdfbe471e299ce825bd4b2c7
Eric Lifka <eric.lifka@gmail.com>
2016-03-08 22:43:56 -0500

    beginnings of a model processer

--- app/services/store.js
+++ app/services/store.js
@@ -4,11 +4,30 @@ const { Service } = Ember;
 
 
 export default Service.extend({
+
+  init() {
+    this._super(...arguments);
+
+    this.set('records', {});
+  },
+
   findAllTags() {
-    return request('/tags');
+    return  request('/tags')
+      .then(data =>
+        this.processRequestData('tags', data));
   },
 
   findAllIngredients() {
-    return request('/ingredients');
+    return request('/ingredients')
+      .then(data =>
+        this.processRequestData('ingredients', data));
+  },
+
+  processRequestData(type, data) {
+    if (!Array.isArray(data)) {
+      data = [ data ];
+    }
+
+    return data.map(record => Ember.Object.create(record));
   }
 });
